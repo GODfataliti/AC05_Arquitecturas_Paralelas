6f32ea9a82ce36092a48b38b4b70a65228a2e288
Fataliti <hell.jnacho@gmail.com>
2021-06-04 12:27:46 -0400

    opc incorrecta agregada

--- filetests/ap-paralela.py
+++ filetests/ap-paralela.py
@@ -9,14 +9,17 @@ primera = clase1()
 segunda = clase2()
 tercera = clase3()
 
+
+# MENU
 def menu():
     while True:
-        print("[1] Ingresar funcion 1")
-        print("[2] Ingresar funcion 2")
-        print("[3] Ingresar funcion 3")
+        print("[1] Ingresar Problema 1")
+        print("[2] Ingresar Problema 2")
+        print("[3] Ingresar Problema 3")
 
         print('Ingrese una opcion: ', end="")
         opc = input()
+        print("")
         if str(opc)=="1":
             print(f"Ingreso {opc}")
             primera.saludar()
@@ -30,13 +33,15 @@ def menu():
             tercera.preguntar()
             break
         else:
+            print("[~] OPCION INCORRECTA [~]\n")
             continue
 
 
 
-
+# MAIN
 def main():
     menu()
+    print("\n")
 
 
 if __name__ == "__main__":
